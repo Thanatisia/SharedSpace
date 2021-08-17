1a1c21462014b73fd8c9e11dde97b306e9298d0b
Thanatisia <55834101+Thanatisia@users.noreply.github.com>
2021-08-17 23:39:56 +0800

    Modified remount.sh to include remounting of other distros

--- Docs/Linux/distros/Debian/Debootstrap/remount.sh
+++ Docs/Linux/distros/Debian/Debootstrap/remount.sh
@@ -35,17 +35,36 @@ fi
 #################
 # Mount Devices #
 #################
-# Mount device partitions to mount path
-mount $device2	/mnt/debinst
-mount $device1	/mnt/debinst/boot
-mount $device3	/mnt/debinst/home
 
-# Temporarily copy mounted fstab
-cp /etc/mtab	/mnt/debinst/etc/mtab
+: "
+- Switch case distro and 
+- mount according to distro
+"
+case "$distro" in
+	"Debian")
+		# Mount device partitions to mount path
+		mount $device2	/mnt/debinst
+		mount $device1	/mnt/debinst/boot
+		mount $device3	/mnt/debinst/home
 
-# Temporarily loan devices from host
-mount -o bind /dev 	/mnt/debinst/dev
-mount -o bind /proc	/mnt/debinst/proc
-mount -o bind /sys	/mnt/debinst/sys
+		# Temporarily copy mounted fstab
+		cp /etc/mtab	/mnt/debinst/etc/mtab
+
+		# Temporarily loan devices from host
+		mount -o bind /dev 	/mnt/debinst/dev
+		mount -o bind /proc	/mnt/debinst/proc
+		mount -o bind /sys	/mnt/debinst/sys
+		;;
+	"Arch")
+		# Mount in Arch ISO
+		mount $device2 /mnt
+		mount $device1 /mnt/boot
+		mount $device3 /mnt/home
+		;;
+	*)
+		# Unknown distros
+		echo "Unknown Distro [$distro]"
+		;;
+esac
 
 # --- Output
